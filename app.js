d901f9bef436e93a038538aa6a20d450e3e10d6e
Keshara Piyumal <keshara.piyumal@oulook.com>
2020-08-08 21:38:32 +0530

    testing

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-// const db = require('./config/database');
-// const assetsDir = require('path').join(__dirname, '/assets');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// // const db = require('./config/database');
+// // const assetsDir = require('path').join(__dirname, '/assets');
 
 
-// Test mySql connection
-// db.authenticate()
-//     .then(() => console.log('MySQL DB Connected..!'))
-//     .catch(err => console.log('Error: ' + err));
+// // Test mySql connection
+// // db.authenticate()
+// //     .then(() => console.log('MySQL DB Connected..!'))
+// //     .catch(err => console.log('Error: ' + err));
 
 
-const app = express();
-const corsOptions = {
-    origin: 'http://localhost:4227',
-    optionsSuccessStatus: 200
-};
+// const app = express();
+// const corsOptions = {
+//     origin: 'http://localhost:4227',
+//     optionsSuccessStatus: 200
+// };
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-// access to assets folder
-// app.use(express.static(assetsDir));
+// // access to assets folder
+// // app.use(express.static(assetsDir));
 
-// routes for the all requests
-// app.use('/users', require('./api/routes/usersR'));
+// // routes for the all requests
+// // app.use('/users', require('./api/routes/usersR'));
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, console.log('Server started on PORT: ' + PORT));
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, console.log('Server started on PORT: ' + PORT));
+console.log('Hello!');
